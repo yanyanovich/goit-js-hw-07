73334f5b0d419b7ec900ce0c0afe877ce19da095
yanyanovich <yan.tokaruk@gmail.com>
2023-02-12 20:01:48 +0200

    finish 2 dz

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const imagesMarkup = ({ original, preview, description }) => {
+  return `
+  <a class="gallery-item" href="${original}">
+  <img class="gallery-image" src="${preview}" alt="${description}" width="340"/>
+  </a>
+  `;
+};
+const createImagesList = galleryItems.map(imagesMarkup).join("");
+galleryEl.insertAdjacentHTML("afterbegin", createImagesList);
+let gallery = new SimpleLightbox(".gallery-item", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
